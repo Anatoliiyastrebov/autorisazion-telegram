e60ce8782156aac9bc6f14b3e1042a0ad90a37a3
Anatoliiyastrebov <anatoliiyastrebov@gmail.com>
2026-02-04 13:52:16 +0100

    Убран устаревший параметр appDir из конфига Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Используем только app/ директорию, игнорируем src/pages
-  experimental: {
-    appDir: true,
-  },
 }
 
 export default nextConfig
